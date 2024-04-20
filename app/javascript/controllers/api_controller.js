3897c3830267f9ca3ee22c2607a4739fe0f2d0b2
Nui Suriya <atchara.suriya@gmail.com>
2024-04-20 22:18:49 +0200

    add basic search bar

--- app/javascript/controllers/api_controller.js
+++ app/javascript/controllers/api_controller.js
@@ -3,6 +3,8 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="api"
 export default class extends Controller {
   connect() {
-    console.log("Connect API controller");
+    // console.log("Connect API controller");
   }
+
+
 }
