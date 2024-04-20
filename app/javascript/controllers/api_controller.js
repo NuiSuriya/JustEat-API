6e575de8081b683cb19137361cc75262e3943cd5
Nui Suriya <atchara.suriya@gmail.com>
2024-04-20 23:25:14 +0200

    fetch data from api

--- app/javascript/controllers/api_controller.js
+++ app/javascript/controllers/api_controller.js
@@ -6,16 +6,24 @@ export default class extends Controller {
 
   connect() {
     // console.log("Connect API controller");
+
   }
 
   search() {
     const url = "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/CB74DL"
     const proxyUrl = "https://cors-anywhere.herokuapp.com/" + url; // Using cors-anywhere as a proxy
-
     fetch(proxyUrl)
       .then(response => response.json())
       .then((data) => {
-        console.log(data);
+        const restaurants = data.restaurants.slice(0,10) // Get first 10
+        restaurants.forEach((restaurant) => {
+          const name = restaurant.name
+          const cuisines = restaurant.cuisines
+          const rating = restaurant.rating.starRating
+          const city = restaurant.address.city
+          const address = restaurant.address.firstLine
+        })
+
       })
   }
 
