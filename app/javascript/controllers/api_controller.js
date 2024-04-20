604cab926b2f83cf58995e049accb1d5a0dad37d
Nui Suriya <atchara.suriya@gmail.com>
2024-04-20 22:34:05 +0200

    connect search button with api controller

--- app/javascript/controllers/api_controller.js
+++ app/javascript/controllers/api_controller.js
@@ -2,9 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="api"
 export default class extends Controller {
+  static targets = ["form", "list"]
+
   connect() {
     // console.log("Connect API controller");
   }
 
+  search() {
+    console.log("searching");
+  }
 
 }
