e7e252ced9d70bce44db2e99e83c154ca50d22e6
Nui Suriya <atchara.suriya@gmail.com>
2024-04-21 02:13:48 +0200

    refractor

--- app/javascript/controllers/api_controller.js
+++ app/javascript/controllers/api_controller.js
@@ -17,8 +17,9 @@ export default class extends Controller {
     fetch(proxyUrl)
       .then(response => response.json())
       .then((data) => {
-        const restaurants = data.restaurants.slice(0,10) // Get first 10
-        let restaurant_list = ""
+        console.log(data);
+        const restaurants = data.restaurants.slice(0, 10) // Get first 10
+        let restaurantHtml = ""
         restaurants.forEach((restaurant) => {
           const name = restaurant.name
           const cuisines = restaurant.cuisines
@@ -26,27 +27,29 @@ export default class extends Controller {
           const city = restaurant.address.city
           const address = restaurant.address.firstLine
           const logo = restaurant.logoUrl
-          console.log(logo);
 
-          restaurant_list += `<div class='col-4'>
-                                  <div class='card mb-3 py-3 px-3 resto-card'>`
-          restaurant_list += `<div class="d-flex">
-                                <img  src='${logo}' alt="logo" class="logo me-4">`
-          restaurant_list += `<h3 class="align-self-center">${name}</h3></div>`
-          restaurant_list += `<p class="mt-3 icon ms-3 mt-4"><i class='fa-solid fa-star'></i> ${rating}</p>`
-          restaurant_list += `<p><span class="icon pe-2 ms-3"><i class="fa-regular fa-location-dot"></i></span> ${address}, ${city}</p>`
-          restaurant_list += "<ul>"
-          cuisines.forEach((cuisine) => {
-            const cuisineName = cuisine.name;
-            restaurant_list += `<li><span class='icon-dark me-2'><i class="fa-solid fa-bowl-food"></i></span>${cuisineName}</li>`
-          })
-          restaurant_list += "</ul></p>"
-
-
-          restaurant_list += `</div></div>`
+          restaurantHtml += buildHtml(logo, name, rating, address, city, cuisines);
         })
-        this.listTarget.innerHTML = restaurant_list
+        this.listTarget.innerHTML = restaurantHtml
       })
+
+    const buildHtml = (logo, name, rating, address, city, cuisines) => {
+      let restaurantHtml = `<div class='col-4'>
+                                  <div class='card mb-3 py-3 px-3 resto-card'>`;
+      restaurantHtml += `<div class="d-flex">
+                                <img  src='${logo}' alt="logo" class="logo me-4">`;
+      restaurantHtml += `<h3 class="align-self-center">${name}</h3></div>`;
+      restaurantHtml += `<p class="mt-3 icon ms-3 mt-4"><i class='fa-solid fa-star'></i> ${rating}</p>`;
+      restaurantHtml += `<p><span class="icon pe-2 ms-3"><i class="fa-regular fa-location-dot"></i></span> ${address}, ${city}</p>`;
+      restaurantHtml += "<ul>";
+      cuisines.forEach((cuisine) => {
+        const cuisineName = cuisine.name;
+        restaurantHtml += `<li><span class='icon-dark me-2'><i class="fa-solid fa-bowl-food"></i></span>${cuisineName}</li>`;
+      });
+      restaurantHtml += "</ul></p>";
+      
+      return restaurantHtml += `</div></div>`;
+    }
   }
 
 }
