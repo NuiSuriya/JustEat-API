33e2a006cbd60efc99467b83f13f987dcb26561e
Nui Suriya <atchara.suriya@gmail.com>
2024-04-23 18:26:45 +0200

    get the postcode fromthe search form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,15 @@
 class PagesController < ApplicationController
   def home
-
+    @end_point = fetch_api
   end
 
   def fetch_api
+    postcode = params[:query]
+    end_point = "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/#{postcode}"
+
+    # @end_point
+    # file = URI.open(end_point).read
+    # doc = JSON.parse(file)
 
   end
 end
